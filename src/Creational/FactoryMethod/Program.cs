87911e3260cb04ac3970d1df28955e9d167fce42
ZippyNeuron <cloudy.andy@icloud.com>
2023-02-09 07:08:16 +0000

    Added missing files

---
+++ src/Creational/FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using FactoryMethod;
+
+AircraftFactory aircraftJetFactory = new AircraftJetFactory();
+IAircraft jetAircraft = aircraftJetFactory.Create();
+
+Console.WriteLine(jetAircraft.Name);
+
+AircraftFactory aircraftPropellerFactory = new AircraftPropellerFactory();
+IAircraft propellerAircraft = aircraftPropellerFactory.Create();
+
+Console.WriteLine(propellerAircraft.Name);
+
+AircraftFactory aircraftDefaultFactory = new AircraftDefaultFactory();
+IAircraft defaultAircraft = aircraftDefaultFactory.Create();
+
+Console.WriteLine(defaultAircraft.Name);
