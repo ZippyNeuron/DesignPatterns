5d123339265635e697c2445f18681c09697bfe15
ZippyNeuron <cloudy.andy@icloud.com>
2023-02-15 19:13:20 +0000

    Added the singleton pattern

---
+++ src/Creational/Singleton/Earth.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton;
+
+public class Earth
+{
+    private static Earth? _instance;
+
+    private Earth()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id;
+    
+    public static Earth Instance => _instance ??= new Earth();
+}
---
+++ src/Creational/Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿using Singleton;
+
+Console.WriteLine($"There is only one Earth -> {Earth.Instance.Id}");
+
+Console.WriteLine($"There is only one Earth -> {Earth.Instance.Id}");
+
+Console.WriteLine($"There is only one Earth -> {Earth.Instance.Id}");
