9d8caddae65728b228f545dab561d3dd67ea902e
ZippyNeuron <cloudy.andy@icloud.com>
2023-02-23 21:03:41 +0000

    Update to car class

--- src/Structural/Flyweight/Car.cs
+++ src/Structural/Flyweight/Car.cs
@@ -5,5 +5,5 @@ public class Car
     public int NoOfDoors { get; set; }
     public bool Manual { get; set; }
     public int NoOfWheels { get; set; }
-    public Engine Engine { get; set; }
+    public Engine? Engine { get; set; }
 }
